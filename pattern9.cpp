03543534eef770eac31bdfcaf7d4622d1faab062
Kaushik <kaushikrawal28@gmail.com>
2021-02-24 23:41:26 +0530

    initial commit

--- pattern9.cpp
+++ pattern9.cpp
@@ -21,37 +21,37 @@ int main()
     int n;
     cin>>n;
 {  
-    int space = n - 1;  
+    int s = n - 1;  
   
     
     for (int i = 0; i < n; i++)  
     {  
      
-        for (int j = 0;j < space; j++)  
+        for (int j = 0;j < s; j++)  
             cout << " ";  
     
         for (int j = 0; j <= i; j++)  
             cout << "* ";  
   
         cout << endl;  
-        space--;  
+        s--;  
     }  
   
       
-    space = 0;  
+    s = 0;  
   
     
     for (int i = n; i > 0; i--)  
     {  
       
-        for (int j = 0; j < space; j++)  
+        for (int j = 0; j < s; j++)  
             cout << " ";  
   
         for (int j = 0;j < i;j++)  
             cout << "* "; 
   
         cout << endl; 
-        space++;  
+        s++;  
     }  
 } 
 return 0; 
